2dbcab1d525436e508be3b50b06af4181a8f464d
bensisa <bsimon@academytrainee.com>
2020-12-17 14:24:34 +0000

    BS: Fully functional Front end

--- ToDoTaskQA/src/main/resources/static/js/create.js
+++ ToDoTaskQA/src/main/resources/static/js/create.js
@@ -32,3 +32,42 @@ document
       )
       alert(`Added New Todo!`)
     }
+
+    document
+    .querySelector("form.createTask")
+    .addEventListener("submit", function (stop) {
+      stop.preventDefault();
+      let formElements = document.querySelector("form.createTask").elements;
+      console.log(formElements)
+      let name=formElements["floatingNameT"].value;
+      let difficulty =formElements["floatingDifficulty"].value;
+      let todoID =formElements["floatingTodo"].value;
+  
+      let data = {
+        "name":name,
+        "difficulty":difficulty,
+        "todo":{
+          "id":todoID
+        }
+    }
+    console.log("Data to post",data)
+    submitTask(data)
+  
+      // postData(noteTitle,noteBody)
+    });
+
+    function submitTask(data){
+    fetch(`http://localhost:8080/task/create`, {
+        method: "post",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify(data),
+      })
+      .then(function(data){
+          console.log("Success!!",data)
+      }
+
+      )
+      alert(`Added New Task!`)
+    }
--- ToDoTaskQA/src/main/resources/static/js/index.js
+++ ToDoTaskQA/src/main/resources/static/js/index.js
@@ -48,6 +48,34 @@ document
       alert(`Deleted ID:${id}`)
     }
 
+    document
+    .querySelector("form.deleteTask")
+    .addEventListener("submit", function (stop) {
+      stop.preventDefault();
+      let formElements = document.querySelector("form.deleteTask").elements;
+      console.log(formElements)
+      let id=formElements["floatingDelID"].value;
+      deleteTaskID(id);
+
+    });
+
+    async function deleteTaskID(id) {
+      let response = await fetch(`http://localhost:8080/task/delete/${id}`, {
+        method: "delete",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+    
+      if (!response.ok) {
+        console.log(
+          `Looks like there was a problem. Status Code: ${response.status}`
+        );
+        return;
+      }
+      alert(`Deleted ID:${id}`)
+    }
+
 
     async function readOne(id) {
       let response = await fetch(`http://localhost:8080/todo/read/${id}`, {
@@ -68,13 +96,34 @@ document
       console.log(data);
     
       let div = document.getElementById("mytable");
-  
-      var row = `<tr>
+      div.innerHTML = '';
+
+      if(data.task.length == 0)
+      {
+        var row = `<tr>
                     <td>${data.id}</td>
                     <td>${data.name}</td>
                     <td>${data.description}</td>
-                 </tr>`
-      div.innerHTML = row;
+                    <td>No Task</td>
+                    <td>No Difficulty</td>
+                  </tr>`
+        div.innerHTML = row;
+      }
+      else{
+        for(var j=0; j < data.task.length; j++)
+        {
+          console.log(data.task[j].name);
+          var row = `<tr>
+                      <td>${data.id}</td>
+                      <td>${data.name}</td>
+                      <td>${data.description}</td>
+                      <td>${data.task[j].name}</td>
+                      <td>${data.task[j].difficulty}</td>
+                    </tr>`
+          div.innerHTML += row;
+        }
+      }
+
     }
 
     async function readName(id) {
@@ -96,15 +145,36 @@ document
       console.log(data);
     
       let div = document.getElementById("mytable");
+      div.innerHTML = '';
+
   
       for(var i=0; i < data.length; i++)
       {
+        if(data[i].task.length == 0)
+        {
           var row = `<tr>
+                      <td>${data[i].id}</td>
+                      <td>${data[i].name}</td>
+                      <td>${data[i].description}</td>
+                      <td>No Task</td>
+                      <td>No Difficulty</td>
+                    </tr>`
+          div.innerHTML += row;
+        }
+        else{
+          for(var j=0; j < data[i].task.length; j++)
+          {
+            console.log(data[i].task[j].name);
+            var row = `<tr>
                         <td>${data[i].id}</td>
                         <td>${data[i].name}</td>
                         <td>${data[i].description}</td>
-                     </tr>`
-          div.innerHTML = row;
+                        <td>${data[i].task[j].name}</td>
+                        <td>${data[i].task[j].difficulty}</td>
+                      </tr>`
+            div.innerHTML += row;
+          }
+        }
       }
     }
 
@@ -135,13 +205,30 @@ document
     div.innerHTML = '';
     for(var i=0; i < data.length; i++)
     {
-      let dataSize = data[i].task
-          var row = `<tr>
-                        <td>${data[i].id}</td>
-                        <td>${data[i].name}</td>
-                        <td>${data[i].description}</td>
-                        <td>${dataSize.id}</td>
-                     </tr>`
+      if(data[i].task.length == 0)
+      {
+        var row = `<tr>
+                    <td>${data[i].id}</td>
+                    <td>${data[i].name}</td>
+                    <td>${data[i].description}</td>
+                    <td>No Task</td>
+                    <td>No Difficulty</td>
+                  </tr>`
         div.innerHTML += row;
+      }
+      else{
+        for(var j=0; j < data[i].task.length; j++)
+        {
+          console.log(data[i].task[j].name);
+          var row = `<tr>
+                      <td>${data[i].id}</td>
+                      <td>${data[i].name}</td>
+                      <td>${data[i].description}</td>
+                      <td>${data[i].task[j].name}</td>
+                      <td>${data[i].task[j].difficulty}</td>
+                    </tr>`
+          div.innerHTML += row;
+        }
+      }
     }
   }
